0387650f7308bb40389cc2392cf5010b9915c59d
Glitch (hello-jquery6) <none>
2020-10-18 20:49:31 +0000

    🏔🖼 Checkpoint ./main.js:22807318/24

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$.getJSON("siliconValley.json", function(json) {
-  var siliconValley = json;
-  console.log(siliconValley);
+$.getJSON("siliconvalley.json", function(json) {
+  var siliconvalley = json;
+  console.log(siliconvalley);
   
-  var scriptedMovie = siliconValley [0];
+  var scriptedMovie = siliconvalley [0];
   console.log(scriptedMovie);
 
   console.log( scriptedMovie.show[0].type );
