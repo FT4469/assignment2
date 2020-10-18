dfb339285745b18d58936e274327ebaa7188c3f0
Glitch (hello-jquery6) <none>
2020-10-18 19:28:29 +0000

    🚏🙍 Checkpoint ./main.js:22807318/1008 ./style.css:22807318/150

--- main.js
+++ main.js
@@ -3,5 +3,14 @@ $.getJSON("siliconValley.json", function(json) {
   console.log(siliconValley);
   
   var movieType = siliconValley [2][4];
-});
+  console.log(movieType);
 
+var col = [];
+  for (var i = 0; i < myBooks.length; i++) {
+    for (var key in myBooks[i]) {
+      if (col.indexOf(key) === -1) {
+        col.push(key);
+      }
+    }
+  }
+}
